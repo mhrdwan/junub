33bcc3b037841c1b886dc958a3d7acc9af5306fc
digitalplatform2erl <147781803+digitalplatform2erl@users.noreply.github.com>
2024-05-10 10:23:21 +0700

    tambah folder doa

--- lib/main.dart
+++ lib/main.dart
@@ -1,5 +1,5 @@
 import 'package:flutter/material.dart';
-import 'package:junub/view/dashboard_view.dart';
+import 'package:junub/screen/dashboard_view.dart';
 
 void main() {
   runApp(const MyApp());
--- lib/screen/Al-Quran/menu1.dart
+++ lib/screen/Al-Quran/menu1.dart
@@ -1,7 +1,7 @@
 import 'package:dio/dio.dart';
 import 'package:flutter/material.dart';
 import 'package:junub/models/surah_quran.dart';
-import 'package:junub/view/Al-Quran/detail-surah.dart';
+import 'package:junub/screen/Al-Quran/detail-surah.dart';
 
 class Menu1 extends StatefulWidget {
   const Menu1({super.key});
